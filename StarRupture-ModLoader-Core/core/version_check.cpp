0ac3c6de5b6f74513b1612abbfd8ce0a3627492e
AlienXAXS <github@smtpd.co.uk>
2026-09-14 18:31:19 +0100

    CL125897

--- StarRupture-ModLoader-Core/core/version_check.cpp
+++ StarRupture-ModLoader-Core/core/version_check.cpp
@@ -12,7 +12,7 @@
 #define MODLOADER_BUILD_TAG "dev"
 #endif
 
-static constexpr wchar_t kRequiredVersionSuffix[] = L"CL-125368";
+static constexpr wchar_t kRequiredVersionSuffix[] = L"CL-125897";
 
 
 // Reads the raw ProductVersion string from the game executable's version info.
